ccd22c13f3dcf1d70c0a6e9c596607c42e3d5836
nirtc1294 <ninjuptk@gmail.com>
2026-05-11 15:03:08 +0530

    vc cvb

--- CheentaProofs/compactnesslemmas.lean
+++ CheentaProofs/compactnesslemmas.lean
@@ -1,3 +1,8 @@
+import Mathlib
 -- File created and edited by Niranjan S Rao. (As of now)
+-- Lemma 1: Every closed subspace of a compact space is also compact
 
--- Lemma 1: Every closed subspace of a compact space
+variable {X : Type*} [TopologicalSpace X]
+variable (A: Set X)
+
+lemma subspaceclosedcompact (hX: IsConnected X) (hA: IsClosed A)
